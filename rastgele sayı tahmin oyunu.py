6859523cee22be594431b31c8b8dea6de78552a1
İbrahim Etem Boz <ibrahimetemboz@gmail.com>
2025-12-16 10:13:23 +0300

    Add files via upload

---
+++ rastgele sayı tahmin oyunu.py
@@ -0,0 +1,12 @@
+import random
+sayı = random.randint(1,100)
+tahmin = -1
+while tahmin != sayı:
+    tahmin = int(input("1 ile 100 arasında bir sayı tahmin edin: "))
+    if tahmin < sayı:
+        print("Daha büyük bir sayı deneyin.")
+    elif tahmin > sayı:
+        print("Daha küçük bir sayı deneyin.")
+    else:
+        print("Tebrikler! Doğru tahmin ettiniz.")   
+        
